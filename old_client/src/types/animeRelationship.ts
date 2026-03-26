03ce1dc8b465e195fb84414c8cca8b9a3e154c05
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2026-03-26 16:02:02 +0800

    remove old nextjs client

--- old_client/src/types/animeRelationship.ts
+++
@@ -1,6 +0,0 @@
-import { RecordModel } from "pocketbase"
-
-export interface AnimeRelationshipRecord extends RecordModel {
-    relationship: string[]
-    user_id: string
-}
--- old_client/src/types/tag.ts
+++
@@ -1,9 +0,0 @@
-import { RecordModel } from "pocketbase"
-
-export interface TagRecord extends RecordModel {
-    name: string
-    color?: string
-    weight?: number
-    user_id: string
-    display: boolean
-}
